3c1f8c783c5bcae8c640a75a691b158aac820bf6
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-03-08 13:29:13 +0530

    Added contact page form

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ContactForm
 
 def home_page(request):
-  return HttpResponse('Hello World')
+  return render(request,'home_page.html',{})
+
+def contact_page(request):
+  contact_form = ContactForm(request.POST or None)
+  context = {'title' : 'Contact',
+            'content' : 'Welcome to Contacts Page',
+            'form' : contact_form 
+            }
+  if contact_form.is_valid():
+    print(contact_form.cleaned_data)
+  # if request.method == 'POST':
+  #   print(request.POST)
+  return render(request,'contact/view.html', context)
+
+def about_page(request):
+  return render(request,'about_page.html',{})
+  

b1b32d6d692a77f150a0d51a6a0ce2e6c2dee695
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-03-08 19:15:17 +0530

    Added support for static cdn for development

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -17,6 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from .views import home_page,about_page,contact_page, login_page, register_page
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^$', home_page),
     url(r'^contact/$', contact_page),
@@ -25,3 +28,7 @@ urlpatterns = [
     url(r'^register/$', register_page),
     url(r'^admin/', admin.site.urls)
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

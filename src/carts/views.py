a370f05ffd483d045713e1b43aee182104d9f27d
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-04-02 07:58:37 +0530

    Created Cart model Manager and updated the cart_home view to update the user field in cart when user moves from anon to logged in

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from .models import Cart
 
-def cart_create(user=None):
-  cart = Cart.objects.create(user=None)
-  return cart
-
 def cart_home(request):
   cart_id = request.session.get('cart_id', None)
   qs = Cart.objects.filter(id=cart_id)
   if qs.count == 1:
     cart_obj = qs.first()
+    if request.user.is_authenticated() and cart_obj.user is None: # Handles the case where if the anon user logs in , the cart user is updated
+      cart_obj.user = request.user
+      cart_obj.save()
   else:
-    cart_obj = cart_create()
+    cart_obj = Cart.objects.new(user=request.user) # Handles both cases of user logged in or anon user
     request.session['cart_id'] = cart_obj.id
   return render(request, 'carts/home.html',{})

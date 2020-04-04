88c9d1b0b7d58a1ab4b7b2aab3e5505d8fd90270
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-04-04 18:21:02 +0530

    Created update-cart form to enable adding and removal of products

--- src/carts/views.py
+++ src/carts/views.py
@@ -13,10 +13,12 @@ def cart_home(request):
   return render(request, 'carts/home.html',{}) 
 
 def cart_update(request):
-  product_obj = Product.objects.get(id=1)
-  cart_obj, new_obj = Cart.objects.new_or_get(request)
-  if product_obj in cart_obj.products.all():
-    cart_obj.products.remove(product_obj)
-  else:
-    cart_obj.products.add(product_obj)
+  product_id = request.POST.get('product_id', None)
+  if product_id is not None:
+    product_obj = Product.objects.get(id=1)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    if product_obj in cart_obj.products.all():
+      cart_obj.products.remove(product_obj)
+    else:
+      cart_obj.products.add(product_obj)
   return redirect("cart:home") 

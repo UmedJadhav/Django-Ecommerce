b5253de627408041c05571a2eb5b342718c2e51a
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-04-05 01:12:26 +0530

    Added db signals to update order total  post cart save

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from carts.models import Cart
 from ecommerce.utils import unique_order_id_generator
 
@@ -23,10 +23,36 @@ class Order(models.Model):
 
   def __str__(self):
     return self.order_id
+  
+  def update_total(self):
+    cart_total = self.cart.total
+    shipping_total = self.shipping_total
+    total = cart_total + shipping_total # Can add taxes and stuff
+    self.total = total
+    self.save()
+    return total
+
    
 def pre_save_created_order_id(sender, instance, *args, **kwargs):
   if not instance.order_id:
     instance.order_id = unique_order_id_generator(instance)
 
+def post_save_cart_total(sender, instance, created, *args, **kwargs):
+  if not created:
+    cart_obj = instance
+    cart_total = cart_obj.total
+    cart_id = cart_obj.id
+    qs = Order.objects.filter(cart__id=cart_id)
+    if qs.count() == 1: # We should have 1 order per cart
+      order_obj = qs.first()
+      order_obj.update_total()
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+  if created:
+    instance.update_total()
+
+
 pre_save.connect(pre_save_created_order_id, sender=Order)
+post_save.connect(post_save_cart_total, sender=Cart)
+post_save.connect(post_save_order, sender=Order)
   

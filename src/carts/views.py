adb88fed256110668980bb8a4ebdd8f8b3d316a1
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-06-17 21:36:26 +0530

    Fixed minor spelling issues

--- src/carts/views.py
+++ src/carts/views.py
@@ -27,8 +27,7 @@ def checkout_home(request):
   cart_obj, cart_created = Cart.objects.new_or_get(request)
   if cart_created or cart_obj.products.count() == 0:
     return redirect('cart:home')
-  else:
-    order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+  order_obj = None
   user = request.user
   billing_profile = None
   login_form = LoginForm()
@@ -42,13 +41,14 @@ def checkout_home(request):
     billing_profile, billling_guest_profile_created = BillingProfile.objects.get_or_create(email=guest_email_obj.email)
   else:
     pass
+  print(billing_profile)
   if billing_profile is not None:
     order_qs = Order.objects.filter(billing_profile=billing_profile, cart=cart_obj, active=True)
     if order_qs.count() == 1:
       order_obj = order_qs.first()
     else:
       old_order_qs = Order.objects.exclude(billing_profile=billing_profile).filter(cart=cart_obj)
-      if old_order_qs.exist():
+      if old_order_qs.exists():
         old_order_qs.update(active=False)
       order_obj = Order.objects.create(billing_profile=billing_profile, cart=cart_obj)
  

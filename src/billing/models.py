0d08c1b28e87144d2dfae2db1156f4da10fce09f
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-06-17 23:35:05 +0530

    Refactored the checkout home by creating BillingProfile  Model Manager

--- src/billing/models.py
+++ src/billing/models.py
@@ -1,19 +1,36 @@
 from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
-
+from accounts.models import GuestEmail
 # Create your models here.
 User = settings.AUTH_USER_MODEL
 
 '''
  An guest email can have 'many' billing profile , but a registered user can have only 1 billing profile
 '''
+
+class BillingProfileManager(models.Manager):
+  def new_or_get(self, request):
+    user = request.user
+    guest_email_id = request.session.get('guest_email_id')
+    billing_profile = None
+    billling_profile_created = False
+    if user.is_authenticated():
+      billing_profile, billling_profile_created = self.model.objects.get_or_create(user=user, email=user.email)
+    elif guest_email_id is not None:
+      guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+      billing_profile, billling_profile_created = self.model.objects.get_or_create(email=guest_email_obj.email)
+    else:
+      pass
+    return billing_profile, billling_profile_created
+
 class BillingProfile(models.Model):
   user = models.OneToOneField(User, null=True, blank=True)
   email = models.EmailField()
   active = models.BooleanField(default=True) 
   timestamp = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
+  objects = BillingProfileManager()
 
   def __str__(self):
     return f"Email: {self.email} Updated_at: {self.updated} Active: {self.active}"
--- src/carts/views.py
+++ src/carts/views.py
@@ -28,19 +28,9 @@ def checkout_home(request):
   if cart_created or cart_obj.products.count() == 0:
     return redirect('cart:home')
   order_obj = None
-  user = request.user
-  billing_profile = None
   login_form = LoginForm()
   guest_form = GuestForm()
-  guest_email_id = request.session.get('guest_email_id')
-
-  if user.is_authenticated():
-    billing_profile, billling_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
-  elif guest_email_id is not None:
-    guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
-    billing_profile, billling_guest_profile_created = BillingProfile.objects.get_or_create(email=guest_email_obj.email)
-  else:
-    pass
+  billing_profile, billling_profile_created = BillingProfile.objects.new_or_get(request)
   print(billing_profile)
   if billing_profile is not None:
     order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)

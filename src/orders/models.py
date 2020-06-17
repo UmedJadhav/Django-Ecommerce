02aa56bf71ec24c8215bc6eba4457a86b5b92d87
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-06-17 22:54:13 +0530

    Refactored the checkout home by creating Order Model Manager

--- src/orders/models.py
+++ src/orders/models.py
@@ -14,6 +14,17 @@ ORDER_STATUS_CHOICES = (
   ('refunded', 'Refunded')
 )
 
+class OrderManager(models.Manager):
+  def new_or_get(self, billing_profile, cart_obj):
+    created = False
+    qs = self.get_queryset().filter(billing_profile=billing_profile, cart=cart_obj, active=True)
+    if qs.count() == 1:
+      order_obj = qs.first()
+    else:
+      order_obj = self.model.objects.create(billing_profile=billing_profile, cart=cart_obj)
+      created = True
+    return order_obj, created
+
 class Order(models.Model):
   order_id = models.CharField(max_length=120, blank=True)
   billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True)
@@ -24,6 +35,7 @@ class Order(models.Model):
   shipping_total = models.DecimalField(default=150.00, max_digits=100, decimal_places=2)
   total = models.DecimalField(default=0.0, max_digits=100, decimal_places=2)
   active = models.BooleanField(default=True)
+  objects = OrderManager()
 
   def __str__(self):
     return self.order_id
@@ -40,6 +52,9 @@ class Order(models.Model):
 def pre_save_created_order_id(sender, instance, *args, **kwargs):
   if not instance.order_id:
     instance.order_id = unique_order_id_generator(instance)
+  qs = Order.objects.filter(cart=instance.cart).exclude(billing_profile=instance.billing_profile)
+  if qs.exists():
+    qs.update(active=False)
 
 def post_save_cart_total(sender, instance, created, *args, **kwargs):
   if not created:

7d722705e3d6f9127c60c38382a44155723f37e6
Umed Jadhav <umed.jadhav@somaiya.edu>
2020-06-25 19:20:44 +0530

    Created basic custom User model in addresses app

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+  email = models.EmailField(max_length=255, unique=True)
+  active = models.BooleanField(default=True)
+  staff = models.BooleanField(default=False)
+  admin = models.BooleanField(default=False) 
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
+
+  def __str__(self):
+    return self.email
+
+  def get_full_name(self):
+    return self.email
+
+  def get_short_name(self):
+    return self.email
+    
+  @property
+  def is_staff(self):
+    return self.staff
+
+  @property
+  def is_admin(self):
+    return self.admin
+  
+  @property
+  def is_active(self):
+    return self.active
+
 class GuestEmail(models.Model):
   email = models.EmailField()
   active = models.BooleanField(default=True)
